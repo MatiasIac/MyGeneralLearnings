8d87dccbb8f2397117573acae083ecc68504a51e
Matias Iacono <matias.iacono@gmail.com>
2016-10-07 10:49:03 -0300

    Factory .Net

---
+++ net/FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using FactoryMethod.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new Factory.Factory();
+            
+            var circle = factory.Create<Circle>();
+            circle.Draw();
+
+            var triangle = factory.Create<Triangle>();
+            triangle.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
