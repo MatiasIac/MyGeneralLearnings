35b16f84c7184bca00f0632843ab751496aa55da
Matias Iacono <matias.iacono@gmail.com>
2016-10-14 16:39:56 -0300

    Prototype

---
+++ net/Prototype/Prototype/Program.cs
@@ -0,0 +1,27 @@
+﻿using Prototype.Prototype;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var color1 = new ClonableColor();
+            color1.ThisColor = Color.FromArgb(100, 100, 100);
+            
+            var color2 = color1.Clone() as ClonableColor;
+
+            Console.WriteLine("Data from cloned: " + color2.ThisColor.ToString());
+
+            color2.ThisColor = Color.FloralWhite;
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ net/Prototype/Prototype/Prototype/ClonableColor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.Prototype
+{
+    public class ClonableColor : Prototype
+    {
+        public Color ThisColor { get; set; }
+
+        public override Prototype Clone()
+        {
+            return this.MemberwiseClone() as ClonableColor;
+        }
+
+    }
+}
