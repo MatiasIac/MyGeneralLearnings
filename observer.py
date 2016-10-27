016e8fc5d901ead16eee98c70a0d300c95d60f30
Matias Herranz <matias.herranz@santexgroup.com>
2016-10-27 16:52:39 -0300

    Observer done.

--- observer.py
+++ observer.py
@@ -27,35 +27,37 @@ class Observer(object):
     def __init__(self, name=None):
         self.name = name or randint(23456, 43567)
 
-    def listen(self, user, message):
+    def listen(self, sender, message):
         """
         Listener receiving a message.
         """
-        print "[{} got msg from '{}'] {}".format(
-            self.name, user, message
+        print "\t(({} got msg from '{}')) --> {}".format(
+            self.name, sender, message
         )
 
 
 ###############################################################################
-# Our observers
-obs1 = Observer(name="obs1")
-obs2 = Observer(name="obs2")
-obs3 = Observer(name="obs3")
-obs4 = Observer(name="obs4")
 
-# Our notifier
-ntf = Notifier()
+if __name__ == "__main__":
+    # Our observers
+    obs1 = Observer(name="obs1")
+    obs2 = Observer(name="obs2")
+    obs3 = Observer(name="obs3")
+    obs4 = Observer(name="obs4")
 
-# Observers for channel #1
-ntf.add_subscriber("ch1", obs1)
-ntf.add_subscriber("ch1", obs2)
-# Observers for channel #2
-ntf.add_subscriber("ch2", obs1)
-ntf.add_subscriber("ch2", obs3)
-# Observers for channel #3
-ntf.add_subscriber("ch3", obs4)
+    # Our notifier
+    ntf = Notifier()
 
-# Let's send some notifications
-ntf.notify(sender="mati", channel="ch1", message="hello, pals!")
-ntf.notify(sender="mati", channel="ch2", message="hello, dudes!!")
-ntf.notify(sender="mati", channel="ch3", message="hello, mate")
+    # Observers for channel #1
+    ntf.add_subscriber("ch1", obs1)
+    ntf.add_subscriber("ch1", obs2)
+    # Observers for channel #2
+    ntf.add_subscriber("ch2", obs1)
+    ntf.add_subscriber("ch2", obs3)
+    # Observers for channel #3
+    ntf.add_subscriber("ch3", obs4)
+
+    # Let's send some notifications
+    ntf.notify(sender="mati", channel="ch1", message="hello, pals!")
+    ntf.notify(sender="mati", channel="ch2", message="hello, dudes!!")
+    ntf.notify(sender="mati", channel="ch3", message="hello, mate")
